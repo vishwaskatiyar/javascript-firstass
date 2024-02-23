197905e3fdc3c33ce265e557fbbb6d9ab7284373
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-02-24 00:08:24 +0530

    First change

---
+++ script.js
@@ -0,0 +1,11 @@
+var divElement= document.getElementById("container");
+divElement.style.color="blue";
+divElement.style.backgroundColor="yellow";
+divElement.style.width="300px";
+divElement.style.height="200px"
+divElement.style.fontWeight="bold";
+divElement.style.padding="10px"
+divElement.style.margin="20px"
+divElement.style.fontSize="18px"
+
+
